ece88a1787110895087e5497b0e1c4c3bc491006
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-06-29 20:59:16 +0530

    Time: 4 ms (50.06%), Space: 13 MB (81.86%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        notebook = {}
+
+        for i, num in enumerate(nums):
+            need = target - num
+
+            if need in notebook:
+                return [notebook[need], i]
+
+            notebook[num] = i
+
+
+
