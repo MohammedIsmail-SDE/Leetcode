78f492ad6d0ee7acbde8d0f638bc79cbd3ad9ee3
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-07-09 22:21:51 +0530

    Time: 3 ms (13.61%), Space: 12.3 MB (51.56%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,32 +1,12 @@
 class Solution(object):
     def check(self, nums):
-        if len(nums) == 1:
-            return True
-
-        # check if already sorted or not
-        def _check_sorted(n):
-            for i in range(1, len(n)):
-                if n[i -1] > n[i]:
-                    return False
-            return True
-         
-        if _check_sorted(nums):
-            return True
-
-        for i in range(len(nums)):
-           el =  nums.pop(0)
-           nums.append(el)
-           if _check_sorted(nums):
-            return True
+        n=len(nums)
+        count=0
+        for i in range(0,n):
+            if nums[i]>nums[(i+1)%n]:
+                count = count+1
+        return count<=1
+    
         
         return False
-
-
-        
-
-'''
-
-
-'''   
-       
         
