c92875b84077f0a897054de3338b757c2478a180
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-07-09 22:20:05 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,8 +1,16 @@
-class Solution(object):
+class Solution:
     def search(self, nums, target):
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-        return -1
-        
-        
+        def binary_search(left, right):
+            if left > right:
+                return -1
+
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return binary_search(mid + 1, right)
+            else:
+                return binary_search(left, mid - 1)
+
+        return binary_search(0, len(nums) - 1) 
