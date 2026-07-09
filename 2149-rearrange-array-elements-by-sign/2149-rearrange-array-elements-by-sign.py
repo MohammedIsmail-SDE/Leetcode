7978d38874f47c1c01b1a64b51203ebfc913b515
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-07-09 22:27:35 +0530

    Time: 39 ms (98.61%), Space: 42.2 MB (88.15%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,16 +1,20 @@
 class Solution(object):
     def rearrangeArray(self, nums):
-        i = 0 
-        j = 1
-        Res = [0]*len(nums)
-        for k  in range(len(nums)):
-            if nums [k] > 0 :
-                Res[i] = nums[k]
-                i += 2
-            else :
-                Res[j] = nums[k]
-                j += 2
-        return Res
 
+        ans = [0] * len(nums)
+
+        pos = 0
+        neg = 1
+
+        for num in nums:
+
+            if num > 0:
+                ans[pos] = num
+                pos += 2
+            else:
+                ans[neg] = num
+                neg += 2
+
+        return ans
         
         
