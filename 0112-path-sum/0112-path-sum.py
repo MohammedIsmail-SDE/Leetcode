eafee96a8b72df71aa02235289d6e5281373fc62
Mohammed Ismail <mohammedismail3220@gmail.com>
2026-07-18 22:11:27 +0530

    Time: 3 ms (46.72%), Space: 14.2 MB (65.87%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -2,9 +2,11 @@ class Solution:
     def hasPathSum(self, root, targetSum):
         if not root:
             return False
-
+        
+        # Check if it's a leaf node
         if not root.left and not root.right:
             return targetSum == root.val
-
+        
+        # Check left and right subtree
         return (self.hasPathSum(root.left, targetSum - root.val) or
                 self.hasPathSum(root.right, targetSum - root.val))
